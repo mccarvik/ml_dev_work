901022cc9450e54ca9f75e6b454be04ce42693eb
mccarvik <mccarviks@gmail.com>
2017-06-26 14:46:08 +0000

    initial commit

---
+++ scripts/run_ml.py
@@ -0,0 +1,107 @@
+import sys
+sys.path.append("/home/ubuntu/workspace/ml_dev_work")
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import datetime, pdb, time
+import numpy as np
+import pandas as pd
+from utils.helper_funcs import timeme
+from utils.data_utils import *
+from utils.db_utils import DBHelper
+from scripts.ml_algorithms import *
+
+
+def run(inputs):
+    # Temp to make testing quicker
+    pdb.set_trace()
+    t0 = time.time()
+    with DBHelper() as db:
+        db.connect()
+        df = db.select('morningstar', where = 'date in ("2010", "2015")')
+    # Getting Dataframe
+    # df = getKeyStatsDataFrame(table='morningstar', date='')
+    t1 = time.time()
+    # app.logger.info("Done Retrieving data, took {0} seconds".format(t1-t0))
+    print("Done Retrieving data, took {0} seconds".format(t1-t0))
+    df = removeUnnecessaryColumns(df)
+    df = timeme(addTarget)(df)
+    df = cleanData(df)
+    df = selectInputs(df, inputs)
+    
+    # timeme(run_perceptron)(df)
+    # timeme(logisticRegression)(df, tuple(inputs))
+    timeme(run_perceptron_multi)(df)
+
+def selectInputs(df, inputs):
+    columns = inputs + ['target'] + ['target_proxy']
+    df = df[columns]
+    return df
+
+def addTarget(df):
+    yr_avg_ret = 5
+    target = []
+    for ind, row in df.iterrows():
+        try:
+            t = df[(df['ticker'] == row['ticker']) & (df['date'] == str(int(row['date']) + yr_avg_ret))]['5yrReturn'].iloc[0]
+            target.append(t)
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            target.append(np.nan)
+    df['target_proxy'] = target
+    df = df.dropna(subset = ['target_proxy'])
+    df = df[df['target_proxy'] != 0]
+    # med = df['target_proxy'].median()
+    # df['target'] = df.apply(lambda x: targetToCat(x['target_proxy'], med), axis=1)
+    breaks = np.percentile(df['target_proxy'], [25, 50, 75])
+    df['target'] = df.apply(lambda x: targetToCatMulti(x['target_proxy'], breaks), axis=1)
+    return df
+
+def targetToCat(x, median):
+    if (x > median):
+        return 1
+    else:
+        return -1
+
+def targetToCatMulti(x, breaks):
+    cat = 0
+    for b in breaks:
+        if x < b:
+            return cat
+        cat += 1
+    return cat
+
+def removeUnnecessaryColumns(df):
+    df = df[RATIOS + KEY_STATS + OTHER +
+            GROWTH + MARGINS + RETURNS +
+            PER_SHARE + INDEX]
+    return df
+
+def cleanData(df):
+    # To filter out errant data
+    df = df[df['trailingPE'] != 0]
+    df = df[df['priceToBook'] > 0]
+    df = df[df['priceToSales'] != 0]
+    df = df[df['divYield'] >= 0]
+    
+    # Temp for training purposes
+    # df = df[abs(df['trailingPE']) < 100]
+    # df = df[abs(df['priceToBook']) < 10]
+    # df = df[df['trailingPE'] > 0]
+    df = df[df['divYield'] > 0]
+    # df = df[df['divYield'] < 10]
+    # df = df[df['debtToEquity'] < 5]
+    df = df[df['returnOnEquity'] > 0]
+    df = df[df['returnOnEquity'] < 50]
+    df = df[df['currentRatio'] < 10]
+    # df = df[df[''] > 0]
+    
+
+    # only look at the top 25 and bottom 25%
+    df = df[(df['target'] == 0) | (df['target'] == 3)]
+    # pdb.set_trace()
+    return df
+
+if __name__ == "__main__":
+    run(['trailingPE', 'priceToBook', 'priceToSales', 'divYield', 'debtToEquity',
+        'returnOnEquity', 'netIncomeMargin', 'freeCashFlowPerShare', 'currentRatio'])
---
+++ utils/db_utils.py
@@ -0,0 +1,151 @@
+import pymysql.cursors
+import pymysql
+import datetime, sys, pdb
+import pandas as pd
+sys.path.append("/home/ubuntu/workspace/ml_dev_work")
+# from app import app
+from utils.helper_funcs import stringify
+
+
+# from config import DBUSER, DBPASSWORD, DB, DBHOST
+DBUSER = 'root'
+DBPASSWORD = ''
+DB = 'finance'
+# DBHOST = 'mccarvik-playground-2305615'
+# DBPASSWORD = 'kmac7272'
+DBHOST = 'localhost'
+
+class DBHelper:
+    ''' Class to help with DB actions
+        This is compatible with a mySQL DB
+    '''
+    def __init__(self):
+        self.cnx = None
+        self.cursor = None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.cursor:
+            self.cursor.close()
+        if self.cnx:
+            self.cnx.close()
+    
+    def connect(self, database=DB, db_user=DBUSER, db_password=DBPASSWORD, db_host=DBHOST):
+        try:
+            self.cnx = pymysql.connect(user=db_user, password=db_password,
+                                  host=db_host,database=database)
+            self.cursor = self.cnx.cursor()
+            # app.logger.info('Successfully connected to ' + database)
+        except Exception as e:
+            # app.logger.info('COULD NOT CONNECT TO ' + database)
+            # app.logger.info("DB ERROR:" + str(e))
+            print("DB ERROR:" + str(e))
+    
+    def select(self, table, cols =['*'], where=None):
+        cols = ",".join(cols)
+        exec_string = '''SELECT {0} 
+                         FROM {1} '''.format(cols,table)
+        if where:
+            exec_string += "WHERE {0}".format(where)
+        try:
+            self.cursor.execute(exec_string)
+            rows = []
+            headers = [h[0] for h in self.cursor.description]
+            for row in self.cursor:
+                rows.append(row)
+            return pd.DataFrame(rows, columns=headers)
+            
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            print("DB SELECT ERROR: {0}, {1}, {2}".format(exc_type, exc_tb.tb_lineno, exc_obj))
+            # app.logger.info("DB SELECT ERROR: {0}, {1}, {2}".format(exc_type, exc_tb.tb_lineno, exc_obj))
+            return {'status': 500}
+    
+    def update(self, table, cols, vals, where):
+        exec_string = 'UPDATE {0}'.format(table)
+        set_string = ' SET '
+        vals =stringify(vals)
+        for c,v in zip(cols, vals):
+            set_string += '{0}={1}, '.format(c,v)
+        set_string = set_string[:-2]
+        exec_string += set_string
+        exec_string += " WHERE {0}".format(where)
+        
+        try:
+            self.cursor.execute(exec_string)
+            self.cnx.commit()
+            return {'status': 200}
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            # import pdb; pdb.set_trace()
+            print("DB UPDATE ERROR: {0}, {1}, {2}".format(exc_type, exc_tb.tb_lineno, exc_obj))
+            # app.logger.info("DB UPDATE ERROR: {0}, {1}, {2}".format(exc_type, exc_tb.tb_lineno, exc_obj))
+            return {'status': 500}
+    
+    def insert_into(self, table, cols, vals):
+        vals = stringify(vals)
+        exec_string = 'INSERT INTO {0} '.format(table)
+        col_string = "(" + ",".join(cols) + ")"
+        val_string = "VALUES (" + ",".join(vals) + ")"
+        exec_string += """
+                        {0}
+                        {1}""".format(col_string, val_string)
+        try:
+            self.cursor.execute(exec_string)
+            self.cnx.commit()
+            return {'status': 200}
+        except pymysql.err.IntegrityError as e:
+            # Duplicate entry for this insert
+            return {'status': 500}
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            # import pdb; pdb.set_trace()
+            print("DB INSERT INTO ERROR: {0}, {1}, {2}".format(exc_type, exc_tb.tb_lineno, exc_obj))
+            # app.logger.info("DB INSERT INTO ERROR: {0}, {1}, {2}".format(exc_type, exc_tb.tb_lineno, exc_obj))
+            return {'status': 500}
+            
+    
+    def upsert(self, table, cols_vals, prim_keys):
+        # first try an insert
+        try:
+            ret = self.insert_into(table, cols_vals.keys(), list(cols_vals.values()))
+            if ret['status'] == 200:
+                self.cnx.commit()
+            else:
+                raise Exception('Error in insert statement (Probably a duplicate')
+        except:
+            try:
+                # if error try update (need to build where clause first)
+                w_c = ""
+                for pk in prim_keys:
+                    w_c += pk + "=" + stringify(cols_vals[pk]) + " AND "
+                w_c = w_c[:-5]
+                self.update(table, cols_vals.keys(), list(cols_vals.values()), w_c)
+                self.cnx.commit()
+                return {'status': 200}
+            except Exception as e:
+                exc_type, exc_obj, exc_tb = sys.exc_info()
+                print("DB UPSERT ERROR: {0}, {1}, {2}".format(exc_type, exc_tb.tb_lineno, exc_obj))
+                # app.logger.info("DB UPSERT ERROR: {0}, {1}, {2}".format(exc_type, exc_tb.tb_lineno, exc_obj))
+                return {'status': 500}
+                
+def restart():
+    import os
+    os.system("sudo /etc/init.d/mysql restart")
+
+def data_dump(table='morningstar'):
+    pdb.set_trace()
+    path = '/home/ubuntu/workspace/finance/app/data/' + table + ".csv"
+    with DBHelper() as db:
+        db.connect()
+        df = db.select(table)
+        df.to_csv(path)
+
+if __name__ == '__main__':
+    # a = DBHelper()
+    # a.connect(db_host='localhost')
+    # a.upsert('eq_screener', {'date':'2015-10-20', 's':'test2', 'n':'test22'}, ['date', 's'])
+    restart()
+    # data_dump()
