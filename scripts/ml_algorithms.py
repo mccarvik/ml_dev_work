416fce31e91db5b66bb01f8cd514f439e2869f03
mccarvik <mccarviks@gmail.com>
2018-03-07 23:04:34 +0000

    working on the adalineGD interesting results

--- scripts/ml_algorithms.py
+++ scripts/ml_algorithms.py
@@ -55,7 +55,6 @@ def run_perceptron(df, xcols, eta=0.1, n_iter=10):
     ppn = Perceptron(eta, n_iter)
     ppn.fit(X_train, y_train.values)
     
-    pdb.set_trace()
     print('Training accuracy:', ppn.score(X_train, y_train))
     print('Test accuracy:', ppn.score(X_test, y_test))
     
@@ -166,16 +165,17 @@ def adalineSGD(df, xcols, eta=0.1, n_iter=10):
     plt.close()
 
 def adalineGD(df, xcols, eta=0.1, n_iter=10):
-    pdb.set_trace()
-    y = df['target']
+    t0 = time.time()
+    # Need this replace to comply with the -1 and 1 of the perceptron binary classifier
+    y = df['target'].replace(0, -1)
     X = df[list(xcols)]
     
-    # standardize features
-    X_std = np.copy(X.values)
-    # X_std[:,0] = (X.values[:,0] - X.values[:,0].mean()) / X.values[:,0].std()
-    # X_std[:,1] = (X.values[:,1] - X.values[:,1].mean()) / X.values[:,1].std()
+    # Standardize and split the training nad test data
+    X_std = standardize(X)
+    ts = 0.3
+    X_train, X_test, y_train, y_test = train_test_split(X_std, y, test_size=ts, random_state=0)
     
-    ada = AdalineGD(n_iter=15, eta=0.00001)
+    ada = AdalineGD(n_iter=15, eta=0.001)
     ada.fit(X_std, y)
     
     plot_decision_regions(X_std, y.values, classifier=ada)
@@ -184,14 +184,14 @@ def adalineGD(df, xcols, eta=0.1, n_iter=10):
     plt.ylabel(list(X.columns)[1])
     plt.legend(loc='upper left')
     plt.tight_layout()
-    plt.savefig(IMG_PATH + 'adaline_2.png', dpi=300)
+    plt.savefig(IMG_ROOT + 'dow/adaline_2.png', dpi=300)
     plt.close()
     
     plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker='o')
     plt.xlabel('Epochs')
     plt.ylabel('Sum-squared-error')
     plt.tight_layout()
-    plt.savefig(IMG_PATH + 'adaline_3.png', dpi=300)
+    plt.savefig(IMG_ROOT + 'dow/adaline_3.png', dpi=300)
     plt.close()
     
 def logisticRegression(df, xcols, C=100, penalty='l2'):
